da02a19183b95fe17df74d02d83ea5c29bdeca60
nozomi hiragi <32630547+nozomi-hiragi@users.noreply.github.com>
2019-07-30 15:02:31 +0900

    ブレンドステート修正

--- VulkanStudy/PipelineFactory.h
+++ VulkanStudy/PipelineFactory.h
@@ -44,12 +44,12 @@ private:
 
     VkPipelineColorBlendAttachmentState color_blend_attachment = {};
     color_blend_attachment.blendEnable         = VK_TRUE;
-    color_blend_attachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;
-    color_blend_attachment.dstColorBlendFactor = VK_BLEND_FACTOR_ONE;
-    color_blend_attachment.colorBlendOp = VK_BLEND_OP_MULTIPLY_EXT;
+    color_blend_attachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
+    color_blend_attachment.dstColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA_SATURATE;
+    color_blend_attachment.colorBlendOp = VK_BLEND_OP_ADD;
     color_blend_attachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
     color_blend_attachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
-    color_blend_attachment.alphaBlendOp = VK_BLEND_OP_MULTIPLY_EXT;
+    color_blend_attachment.alphaBlendOp = VK_BLEND_OP_ADD;
     color_blend_attachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
 
     VkPipelineColorBlendAttachmentState color_blend_attachments[] = {
