3f0147839233e3ca1c07451c4db336342a3be201
nozomi hiragi <32630547+nozomi-hiragi@users.noreply.github.com>
2019-08-09 22:06:41 +0900

    インスタンスのエクステンション追加に対応

--- VulkanStudy/InstanceFactory.cpp
+++ VulkanStudy/InstanceFactory.cpp
@@ -8,7 +8,7 @@
 constexpr auto ENGINE_NAME = "Speell";
 constexpr auto ENGINE_VERSION = 0;
 
-const VkInstance InstanceFactory::_createVkInstance(const char* app_name, const uint32_t app_version) {
+const VkInstance InstanceFactory::_createVkInstance(const char* app_name, const uint32_t app_version, const std::vector<const char*>& extensions) {
   VkApplicationInfo app_info = {};
   app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
   app_info.pApplicationName = app_name;
@@ -17,17 +17,33 @@ const VkInstance InstanceFactory::_createVkInstance(const char* app_name, const 
   app_info.engineVersion = ENGINE_VERSION;
   app_info.apiVersion = VK_API_VERSION_1_0;
 
-  // Layers verification
   std::vector<const char*> required_instance_layers;
+  std::vector<const char*> required_instance_extensions;
+
 #ifdef _DEBUG
+  required_instance_layers.push_back("VK_LAYER_GOOGLE_threading");
   required_instance_layers.push_back("VK_LAYER_LUNARG_standard_validation");
+  required_instance_layers.push_back("VK_LAYER_LUNARG_parameter_validation");
+  required_instance_layers.push_back("VK_LAYER_LUNARG_object_tracker");
+  required_instance_layers.push_back("VK_LAYER_LUNARG_image");
+  required_instance_layers.push_back("VK_LAYER_LUNARG_core_validation");
+  required_instance_layers.push_back("VK_LAYER_LUNARG_swapchain");
+  required_instance_extensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
 #endif // DEBUG
 
+  uint32_t glfw_extensions_count = 0;
+  const char** glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extensions_count);
+  for (uint32_t i = 0; i < glfw_extensions_count; i++) {
+    required_instance_extensions.push_back(glfw_extensions[i]);
+  }
+  required_instance_extensions.reserve(required_instance_extensions.size() + extensions.size());
+  std::copy(extensions.begin(), extensions.end(), std::back_inserter(required_instance_extensions));
+
+  // Layers verification
   uint32_t layer_count = 0;
   auto result = vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
   std::vector<VkLayerProperties> available_instance_layers(layer_count);
   result = vkEnumerateInstanceLayerProperties(&layer_count, available_instance_layers.data());
-
   std::vector<const char*> enabled_instance_layers;
   for (auto required : required_instance_layers) {
     for (auto allowed : available_instance_layers) {
@@ -39,18 +55,30 @@ const VkInstance InstanceFactory::_createVkInstance(const char* app_name, const 
   }
 
   // Extensions verification
-  uint32_t extensions_count = 0;
-  const char** extensions = glfwGetRequiredInstanceExtensions(&extensions_count);
+  uint32_t extension_count = 0;
+  result = vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
+  std::vector<VkExtensionProperties> available_instance_extensions(extension_count);
+  result = vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, available_instance_extensions.data());
+  std::vector<const char*> enabled_instance_extensions;
+  for (auto required : required_instance_extensions) {
+    for (auto allowed : available_instance_extensions) {
+      if (strcmp(required, allowed.extensionName) == 0) {
+        enabled_instance_extensions.push_back(required);
+        break;
+      }
+    }
+  }
 
   VkInstanceCreateInfo instance_info = {};
   instance_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
   instance_info.pApplicationInfo = &app_info;
   instance_info.enabledLayerCount = static_cast<uint32_t>(enabled_instance_layers.size());
   instance_info.ppEnabledLayerNames = enabled_instance_layers.data();
-  instance_info.enabledExtensionCount = extensions_count;
-  instance_info.ppEnabledExtensionNames = extensions;
+  instance_info.enabledExtensionCount = static_cast<uint32_t>(enabled_instance_extensions.size());
+  instance_info.ppEnabledExtensionNames = enabled_instance_extensions.data();
 
-  VkInstance instance;
-  result = vkCreateInstance(&instance_info, nullptr, &instance);
-  return instance;
+  VkInstance vk_instance;
+  result = vkCreateInstance(&instance_info, nullptr, &vk_instance);
+
+  return vk_instance;
 }
--- VulkanStudy/InstanceFactory.h
+++ VulkanStudy/InstanceFactory.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
+#include <iostream>
 
 #include "AbstractFactory.h"
 #include "instanceObject.h"
 #include "PhysicalDeviceObject.h"
 
-class InstanceFactory : public AbstractFactory<InstanceObject, void, const char*, const uint32_t> {
+class InstanceFactory : public AbstractFactory<InstanceObject, void, const char*, const uint32_t, const std::vector<const char*>&> {
 public:
   InstanceFactory() {
   }
@@ -14,9 +15,17 @@ public:
   ~InstanceFactory() {
   }
 
+  auto createObject(const std::shared_ptr<void> parent, const char* app_name, const uint32_t app_version, const std::vector<const char*>& extensions) {
+    return AbstractFactory::createObject(parent, app_name, app_version, extensions);
+  }
+
+  auto createObject(const std::shared_ptr<void> parent, const char* app_name, const uint32_t app_version) {
+    return AbstractFactory::createObject(parent, app_name, app_version, std::vector<const char*>());
+  }
+
 protected:
 private:
-  static const VkInstance _createVkInstance(const char* app_name, const uint32_t app_version);
+  static const VkInstance _createVkInstance(const char* app_name, const uint32_t app_version, const std::vector<const char*>& extensions);
 
   static const void _destroyVkInstance(VkInstance instance) {
     vkDestroyInstance(instance, nullptr);
@@ -31,8 +40,8 @@ private:
     return std::move(physical_devices);
   }
 
-  std::shared_ptr<InstanceObject> _createCore(const char* app_name, const uint32_t app_version) {
-    auto vk_instance = _createVkInstance(app_name, app_version);
+  std::shared_ptr<InstanceObject> _createCore(const char* app_name, const uint32_t app_version, const std::vector<const char*>& extensions) {
+    auto vk_instance = _createVkInstance(app_name, app_version, extensions);
     auto vk_physical_devices = _getVkPhysicalDevices(vk_instance);
 
     std::vector<std::shared_ptr<PhysicalDeviceObject>> devices;
@@ -53,10 +62,57 @@ private:
       devices.push_back(physical_device_object);
     }
 
+#ifdef _DEBUG
+    _vkCreateDebugReportCallbackEXT = (PFN_vkCreateDebugReportCallbackEXT)vkGetInstanceProcAddr(vk_instance, "vkCreateDebugReportCallbackEXT");
+    _vkDestroyDebugReportCallbackEXT = (PFN_vkDestroyDebugReportCallbackEXT)vkGetInstanceProcAddr(vk_instance, "vkDestroyDebugReportCallbackEXT");
+
+    VkDebugReportCallbackCreateInfoEXT debug_report_callback_info = { VK_STRUCTURE_TYPE_DEBUG_REPORT_CREATE_INFO_EXT };
+    debug_report_callback_info.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT;
+    debug_report_callback_info.pfnCallback = [](
+      VkDebugReportFlagsEXT flags,
+      VkDebugReportObjectTypeEXT object_type, uint64_t object,
+      size_t location, int32_t message_code, const char* layer_prefix,
+      const char *message, void *user_data) -> VkBool32 {
+      switch (flags) {
+      case VK_DEBUG_REPORT_ERROR_BIT_EXT:
+        std::cout << "VK ERROR " << layer_prefix << " " << location << ":" << message_code << ": " << message << std::endl;
+        break;
+      case VK_DEBUG_REPORT_WARNING_BIT_EXT:
+        std::cout << "VK WARNING " << layer_prefix << " " << location << ":" << message_code << ": " << message << std::endl;
+        break;
+      case VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT:
+        std::cout << "VK PERF " << layer_prefix << " " << location << ":" << message_code << ": " << message << std::endl;
+        break;
+      case VK_DEBUG_REPORT_INFORMATION_BIT_EXT:
+        std::cout << "VK INFO " << layer_prefix << " " << location << ":" << message_code << ": " << message << std::endl;
+        break;
+      case VK_DEBUG_REPORT_DEBUG_BIT_EXT:
+        std::cout << "VK DEBUG " << layer_prefix << " " << location << ":" << message_code << ": " << message << std::endl;
+        break;
+      default:
+        break;
+      }
+      return VK_FALSE;
+
+    };
+    debug_report_callback_info.pUserData = nullptr;
+
+    _vkCreateDebugReportCallbackEXT(vk_instance, &debug_report_callback_info, nullptr, &_vk_debug_report_callback);
+#endif // DEBUG
+
     return std::make_shared<InstanceObject>(vk_instance, std::move(devices));
   }
 
   void _destroyCore(std::shared_ptr<InstanceObject> object) {
+#ifdef _DEBUG
+    _vkDestroyDebugReportCallbackEXT(object->_vk_instance, _vk_debug_report_callback, nullptr);
+#endif // DEBUG
     _destroyVkInstance(object->_vk_instance);
   }
+
+#ifdef _DEBUG
+  PFN_vkCreateDebugReportCallbackEXT _vkCreateDebugReportCallbackEXT = nullptr;
+  PFN_vkDestroyDebugReportCallbackEXT _vkDestroyDebugReportCallbackEXT = nullptr;
+  VkDebugReportCallbackEXT _vk_debug_report_callback;
+#endif // DEBUG
 };
